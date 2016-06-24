0a9fca2b861e167f22b7053158da108ebc70dc85
Ido Green <greenido@gmail.com>
2016-06-24 17:34:32 +0300

    minor tuning to formating

--- js/main.js
+++ js/main.js
@@ -1434,8 +1434,8 @@
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
-    var hour = a.getHours();
     var min = a.getMinutes();
+    var hour = a.getHours();
     var sec = a.getSeconds();
     var time = date + '/' + month + '/' + year + ' - ' + hour + ':' + min + ':' + sec;
     return time;
