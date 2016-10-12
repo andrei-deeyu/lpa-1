e490fbbeddf0cfcfc8d563ea06cb6b03631c4009
Ewa Gasperowicz <devnook@gmail.com>
2016-10-12 23:25:12 +0200

    Changing the template to use MDL. Rewriting mentor app to remove jquery and upgrade to firebase 3.0.4

---
+++ js/auth.js
@@ -0,0 +1,25 @@
+var authModule = (function(firebaseAuth) {
+  let auth = firebaseAuth();
+  let provider = new firebaseAuth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/userinfo.email');
+
+  return {
+    authWithGoogle: function() {
+      auth.signInWithPopup(provider).then(function(result) {
+        // User signed in!
+      }).catch(function(error) {
+        // An error occurred
+        console.log(error);
+      });
+    },
+
+    signOut: function(e) {
+      firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }, function(error) {
+        // An error happened.
+      });
+    }
+
+  };
+})(firebase.auth);
---
+++ js/main-mentor-new.js
@@ -0,0 +1,107 @@
+/**
+ * JS for the mentor web app
+ * Author: Ido Green, Ewa Gasperowicz
+ * Date: 10/2016
+ * V0.9
+ * A 🐱 App
+ *
+ * TODO: Add Analytics support.
+ * TODO: Add Transition animations.
+ * TODO: Use ES6 modules.
+ */
+(function(firebase) {
+
+  /**
+   * UI Elements cache.
+   */
+  const ELEMENTS = {
+    signOut: document.getElementById('lpa-sign-out'),
+    signIn: document.getElementById('lpa-sign-in'),
+    signInSection: document.getElementById('lpa-sign-in-section'),
+    mentorsListTemplate: document.getElementById('lpa-mentors-list-template'),
+    mentorsList: document.getElementById('lpa-mentors-list'),
+    attendeesListTemplate: document.getElementById('lpa-attendees-list-template'),
+    attendeesList: document.getElementById('lpa-attendees-list'),
+    mainNav: document.getElementById('lpa-main-nav')
+  };
+
+  /**
+   * Component responsible for HTML modification.
+   */
+  let UI = {
+    updateUser: function(user) {
+      if (user) {
+        ELEMENTS.signOut.style.display = 'block';
+        // TODO: Animate sign in section.
+        ELEMENTS.signInSection.style.display = 'none';
+      } else {
+        ELEMENTS.signOut.style.display = 'none';
+        ELEMENTS.signInSection.style.display = 'block';
+        ELEMENTS.mentorsList.innerHTML = '';
+      }
+    },
+    updateMentorsList: function(mentorSnapshots) {
+      ELEMENTS.mentorsList.innerHTML = '';
+      if (mentorSnapshots) {
+        mentorSnapshots.forEach(function(mentorSnapshot) {
+          let node = ELEMENTS.mentorsListTemplate.cloneNode(true);
+          node.removeAttribute('id');
+          node.classList.remove('lpa-template');
+          node.querySelector('[data-field="name"]').innerText = mentorSnapshot.val().name;
+          node.querySelector('[data-field="bio"]').innerText = mentorSnapshot.val().bio;
+          ELEMENTS.mentorsList.appendChild(node);
+        });
+      }
+    },
+    updateAttendeesList: function(attendeeSnapshots) {
+      ELEMENTS.attendeesList.innerHTML = '';
+      if (attendeeSnapshots) {
+        attendeeSnapshots.forEach(function(attendeeSnapshot) {
+          let node = ELEMENTS.attendeesListTemplate.cloneNode(true);
+          node.removeAttribute('id');
+          node.classList.remove('lpa-template');
+          node.querySelector('[data-field="name"]').innerText = attendeeSnapshot.val().name;
+          ELEMENTS.attendeesList.appendChild(node);
+        });
+      }
+    },
+    showSubpage: function(subpageName) {
+      document.querySelectorAll('.lpa-subpage').forEach(function(subpage) {
+        subpage.style.display = 'none';
+      });
+      let subpage = document.getElementById('lpa-' + subpageName + '-subpage');
+      subpage.style.display = 'block';
+    }
+  };
+
+  /**
+   * Event listeners.
+   */
+  ELEMENTS.mainNav.addEventListener('click', function(e) {
+    e.preventDefault();
+    let subpageName = e.target.getAttribute('data-subpage');
+    if (subpageName) {
+      UI.showSubpage(subpageName);
+    }
+  });
+  ELEMENTS.signIn.addEventListener('click', authModule.authWithGoogle);
+  ELEMENTS.signOut.addEventListener('click', function(e) {
+    e.preventDefault();
+    authModule.signOut();
+  });
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    UI.updateUser(user);
+    if (user) {
+      // Fetch mentors from the backend.
+      firebase.database().ref('mentors/').once('value').then(function(snapshot) {
+        UI.updateMentorsList(snapshot);
+      });
+      // Fetch attendees from the backend.
+      firebase.database().ref('attendees/').once('value').then(function(snapshot) {
+        UI.updateAttendeesList(snapshot);
+      });
+    }
+  });
+
+})(firebase, authModule);
