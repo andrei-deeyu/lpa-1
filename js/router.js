6fe5060b7426166c858c34c35bc89b74f017fff7
Ewa Gasperowicz <devnook@gmail.com>
2016-12-07 14:32:37 +0100

    Adding router

---
+++ js/router.js
@@ -0,0 +1,56 @@
+/**
+ * JS for the mentor web app
+ * Author: Ido Green, Ewa Gasperowicz
+ * Date: 10/2016
+ * V0.9
+ * A 🐱 App
+ *
+ * TODO: Add Analytics support.
+ * TODO: Add Transition animations.
+ * TODO: Use ES6 modules.
+ */
+var router = (function(firebaseApi, authModule) {
+
+  const BASE_URL = '/index-mentor-new.html';
+
+
+
+  function getParentNodeByType(el, nodeType) {
+    while (el && el.tagName !== nodeType) {
+       el = el.parentNode;
+    }
+    return el;
+  };
+
+
+
+  function navigate(e) {
+    e.preventDefault();
+    let linkEl = getParentNodeByType(e.target, 'A');
+    let subpageName = linkEl.getAttribute('data-subpage');
+    if (subpageName) {
+      window.history.pushState(null, null, BASE_URL + '/' + subpageName);
+      UI.showSubpage(subpageName);
+    }
+  };
+
+  let router = {
+    go: (subpageName, itemKey) => {
+      let name = (subpageName === 'startups' && itemKey) ? 'startup' : subpageName;
+      let subpage = UI.showSubpage(name);
+      let initPage = subpage.getAttribute('data-init');
+      if (initPage) {
+        console.log(firebaseApi.CACHE)
+        let item = firebaseApi.CACHE[subpageName][itemKey];
+        UI[initPage](item);
+      }
+    }
+  };
+
+  window.addEventListener("popstate", function(e) {
+    let urlParts = window.location.pathname.split('/');
+    router.go(urlParts[2], urlParts[3]);
+  }, false);
+
+  return router;
+})(firebaseApi, authModule);
