4b5b30864828fe593243a28091648c121de83ab5
Ido Green <greenido@gmail.com>
2017-02-04 19:57:55 -0800

    set the right config to the mentor PWA

--- mentor/js/firebase.js
+++ mentor/js/firebase.js
@@ -179,10 +179,11 @@ var firebaseApi = (function() {
      storageBucket: "lpa-3-14341.appspot.com",
   };*/
   var config = {
-    apiKey: "AIzaSyDImJzAqBmZVXdaK55jVfRuoaHVLBDFgxU",
-    authDomain: "lpa-1.firebaseapp.com",
-    databaseURL: "https://lpa-1.firebaseio.com",
-    storageBucket: "project-1969056342883930904.appspot.com",
+    apiKey: "AIzaSyBZWmUQWKU1vs--CMbITX8lawdmCslHaJs",
+    authDomain: "lpa-space.firebaseapp.com",
+    databaseURL: "https://lpa-space.firebaseio.com",
+    storageBucket: "lpa-space.appspot.com",
+    messagingSenderId: "942783265132"
   };
   firebase.initializeApp(config);
 
